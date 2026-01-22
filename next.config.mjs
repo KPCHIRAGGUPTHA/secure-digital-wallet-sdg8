287b8a7e0db2234393d87386a409d64525943dbc
KPCHIRAGGUPTHA <chiragkpguptha@gmail.com>
2026-01-22 11:01:20 +0530

    Configure basePath for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+
+  basePath: "/secure-digital-wallet-sdg8",
+  assetPrefix: "/secure-digital-wallet-sdg8/",
+
   typescript: {
     ignoreBuildErrors: true,
   },
+
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
